c824c9da7014284ce5bf701db7ba243a4db76ea6
Mohammed Ifkirne <161834771+MohaIfk@users.noreply.github.com>
2026-02-20 15:08:40 +0000

    add: struct client

--- src/client.py
+++ src/client.py
@@ -1,10 +1,14 @@
 import socket
+import struct
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     server_ip_port = input("Enter server IP: ")
     i = server_ip_port.index(":")
     print("Connecting to server...", (server_ip_port[:i], int(server_ip_port[i+1:])))
     s.connect((server_ip_port[:i], int(server_ip_port[i+1:])))
-    s.sendall(b"https://fs.uit.ac.ma/")
+    url = b"https://fs.uit.ac.ma/"
+    data = struct.pack(">BH", 1, len(url))
+    data += url
+    s.sendall(data)
     data=s.recv(2048)
     print(data)
